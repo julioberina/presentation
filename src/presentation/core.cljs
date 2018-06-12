72d98f6806b9dafd309e616bdfbc2b539c6adbfd
Julio Berina <julioberina@gmail.com>
2018-06-11 20:10:37 -0700

    Added a page for slides

--- src/presentation/core.cljs
+++ src/presentation/core.cljs
@@ -1,13 +1,13 @@
 (ns presentation.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+    (:require [reagent.core :as reagent :refer [atom]]
+              [presentation.slides :as slide]))
 
 (enable-console-print!)
 
 (defonce state (atom {}))
 
 (defn hello-world []
-  [:div
-   [:h1 "Presentation coming soon..."]])
+  (slide/title-slide))
 
 (reagent/render-component [hello-world]
                           (.getElementById js/document "app"))
---
+++ src/presentation/slides.cljs
@@ -0,0 +1,6 @@
+(ns presentation.slides)
+
+(defn title-slide []
+  [:div
+   [:h1 "Intro to Clojure"]
+   [:p "Julio Berina"]])
