c71f34f5c399dbe0238005c06f7c5fd01cfbf1fb
Julio Berina <julioberina@gmail.com>
2018-06-12 20:49:35 -0700

    Got slide toggle working

--- src/presentation/core.cljs
+++ src/presentation/core.cljs
@@ -1,13 +1,49 @@
 (ns presentation.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [presentation.slides :as ps]))
+    (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(defonce state (atom {}))
+;; Begin functions to toggle slides
+
+(defn next-slide [vslides]
+  (conj (vec (rest vslides)) (first vslides)))
+
+(defn prev-slide [vslides]
+  (vec (cons (last vslides) (butlast vslides))))
+
+(defn change-slide [kcode slide-vector]
+  (cond
+    (= kcode 32) (reset! slide-vector (next-slide @slide-vector))
+    (= kcode 8) (reset! slide-vector (prev-slide @slide-vector)))
+  (first @slide-vector))
+
+;; End functions to toggle slides
+
+;; Slides
+
+(defn title-slide []
+  [:div
+   [:h1 "Intro to Clojure"]
+   [:p "Julio Berina"]])
+
+(defn why-clojure-slide []
+  [:div
+   [:h1 "Why Clojure?"]])
+
+;; Vector of slides
+
+(def slide-vector
+  (atom (vector title-slide why-clojure-slide)))
 
 (defn main-presentation []
-  (ps/display ps/title-slide))
+  (let [slide (atom (first @slide-vector))]
+    (fn []
+      [:div.container
+       {:tab-index "0"
+        :on-key-up (fn [event] (reset! slide (change-slide (.-keyCode event) slide-vector)))}
+       [:div.slide
+        [:div.content
+         (@slide)]]])))
 
 (reagent/render-component [main-presentation]
                           (.getElementById js/document "app"))
